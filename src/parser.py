3bdaaa8d9e6201998fdcb53424ab859f5fe24b78
David Russell <druss337@gmail.com>
2022-05-01 11:44:36 -0600

    parser class

--- src/parser.py
+++ src/parser.py
@@ -1,2 +1,23 @@
 #import to zmapReporter
 #parse banner outputs to dataframes
+
+
+class Parser:
+
+    def __init__(self, working_dir_path):
+        self.working_dir = working_dir_path
+
+    def load_banners(self):
+        '''
+        Load banner json files generated from zgrab
+
+        returns: loaded banners
+        '''
+    
+    def parse_banners(self):
+        '''
+        Parse loaded banners to pandas dataframes with select features
+
+        returns: dict of dataframes
+        '''
+

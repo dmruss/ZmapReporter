a40efe4422e7f05a2bb275a9495a7c52430ba7d1
RYNO5 <104735753+RYNO5@users.noreply.github.com>
2022-05-01 22:08:16 -0600

    Update zscript.py

--- src/zscript.py
+++ src/zscript.py
@@ -1,2 +1,8 @@
 #import to zmapReporter
 #run zmap/zgrab
+import os
+
+
+def runZmap(a, b):
+    os.system("cd")
+    os.system("sudo zmap -p " + str(a) + " -N " + str(b) + "| ztee maybeSSH.csv | ~/go/pkg/mod/github.com/zmap/zgrab2@v0.1.7/zgrab2 ssh -o banners.json")

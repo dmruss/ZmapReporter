36f2076ba7d7c6d5bf5eee506e3475eb3a1b160d
RYNO5 <104735753+RYNO5@users.noreply.github.com>
2022-05-02 13:53:55 -0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,23 +4,29 @@
 sudo apt-get update
 
 #install python
-sudo apt-get install python3
+neg = " no "
+path = which python3
+if neg in path:
+  sudo apt-get install python3
 
 #install python dependencies
 pip install -r requirements.txt
 
 ##also install zmap and zgrab
-sudo apt-get install zmap
-sudo apt-get install golang-go
+path = which zmap
+if neg in path:
+  sudo apt-get install zmap
+path = which go
+if neg in path:
+  sudo apt-get install golang-go
 cd || exit
-echo "Moved to Base Directory ***************************************************************************************"
 mkdir ZmapReporter
-echo "Created Zmap Reporter Directory********************************************************************************"
 cd ZmapReporter || exit
-echo "Entered Zmap Reporter Directory *******************************************************************************"
-sudo go get github.com/zmap/zgrab2
-echo "Retrieved Zgrab2 Repository ***********************************************************************************"
+path = which zgrab2
+if neg in path:
+  sudo go get github.com/zmap/zgrab2
 cd || exit
-cd go/pkg/mod/github.com/zmap/zgrab2@v0.1.7 || exit
+path = find ~ -type d -name "zgrab2@v0.1.7"
+cd path || exit
 sudo go get github.com/stretchr/testify
 make

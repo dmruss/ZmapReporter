03abed5d5bb0427681768085433999391e4c1c6e
RYNO5 <104735753+RYNO5@users.noreply.github.com>
2022-05-01 20:34:29 -0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -12,9 +12,15 @@ pip install -r requirements.txt
 ##also install zmap and zgrab
 sudo apt-get install zmap
 sudo apt-get install golang-go
+cd || exit
+echo "Moved to Base Directory ***************************************************************************************"
 mkdir ZmapReporter
-cd ZmapReporter
+echo "Created Zmap Reporter Directory********************************************************************************"
+cd ZmapReporter || exit
+echo "Entered Zmap Reporter Directory *******************************************************************************"
 sudo go get github.com/zmap/zgrab2
-cd
-cd go/pkg/mod/github.com/zmap/zgrab2@v0.1.7
+echo "Retrieved Zgrab2 Repository ***********************************************************************************"
+cd || exit
+cd go/pkg/mod/github.com/zmap/zgrab2@v0.1.7 || exit
 sudo go get github.com/stretchr/testify
+make
